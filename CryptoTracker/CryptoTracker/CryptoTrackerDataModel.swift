2ce9efaa76c452f08756a52f7bce1eba36b958cf
JustinCaty <justincaty@Justins-Mac-mini.local>
2019-02-13 21:38:51 -0500

    adding storyboard updates and adding service calls to pull data and set data into tableview with search bar to filter data

---
+++ CryptoTracker/CryptoTracker/CryptoTrackerDataModel.swift
@@ -0,0 +1,88 @@
+//
+//  CryptoTrackerDataModel.swift
+//  CryptoTracker
+//
+//  Created by JustinCaty on 2/12/19.
+//  Copyright © 2019 JustinCaty. All rights reserved.
+//
+
+import UIKit
+
+struct CoinList: Decodable {
+    var Message: String
+    var Data: Dictionary<String, CoinName>
+}
+
+struct CoinName: Decodable {
+    var CoinName: String
+    var Symbol: String
+    var ImageUrl: String?
+}
+
+struct CoinPrice: Decodable {
+    var USD: Double?
+}
+
+//struct CryptoTracking: Decodable {
+//    var Data: [CryptoData]
+//}
+//
+//struct CryptoData: Decodable {
+//    var CoinInfo: CryptoCoinInfo
+//    var DISPLAY: CryptoCoinPricingInfoFormation
+//
+//}
+//
+//struct CryptoCoinInfo: Decodable {
+//    var Name: String
+//    var ImageUrl: String
+//}
+//
+//struct CryptoCoinPricingInfoFormation: Decodable {
+//    var USD: USDInformation
+//}
+//
+//struct USDInformation: Decodable {
+//    var PRICE: String
+//}
+
+class CryptoTrackerDataModel {
+    
+    func getCoinList(completionHandler: @escaping (CoinList) -> ()) {
+        guard let url = URL(string: "https://min-api.cryptocompare.com/data/all/coinlist?api_key=00bd6c061026737fd4009bff205f66eaa9fa588f558ee71f9170ac3a51535f21") else { return }
+        let session = URLSession.shared
+                session.dataTask(with: url) { (data, response, error) in
+                    if let data = data {
+                        do {
+                            let results = try JSONDecoder().decode(CoinList.self, from: data)
+                            completionHandler(results)
+                        } catch {
+                            print("Error \(error)")
+                        }
+                    }
+        
+                }.resume()
+    }
+    
+    func coinPrice(symbol: String, completionHandler: @escaping (CoinPrice) -> ()) {
+        guard let url = URL(string:  "https://min-api.cryptocompare.com/data/price?fsym=\(symbol)&tsyms=USD&api_key=00bd6c061026737fd4009bff205f66eaa9fa588f558ee71f9170ac3a51535f21") else { return }
+            let session = URLSession.shared
+            session.dataTask(with: url) { (data, response, error) in
+                if let data = data {
+                    do {
+                        let results = try JSONDecoder().decode(CoinPrice.self, from: data)
+                        completionHandler(results)
+                    } catch {
+                        print("Error \(error)")
+                    }
+                }
+                
+                }.resume()
+    }
+
+    func getImageFromUrl(imageURL: String) -> Data  {
+        guard let iconURL = URL(string: "https://www.cryptocompare.com\(imageURL)"),
+            let data = try? Data(contentsOf:iconURL) else { fatalError() }
+        return data
+    }
+}
--- CryptoTracker/CryptoTracker/ViewController.swift
+++ CryptoTracker/CryptoTracker/ViewController.swift
@@ -8,13 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:  UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    var cryptoTrackerData = CryptoTrackerDataModel()
+    var results: CoinList?
+    var coinList: [CoinName]?
+    var sortedCoinList: [CoinName]?
+    var filterdCoinList: [CoinName]?
+    var isSearching: Bool = false
+    
+    //@IBOutlet weak var coinListPicker: UIPickerView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        coinListPicker.delegate = nil
+//        coinListPicker.dataSource = nil
+        loadCryptoData()
     }
-
-
+    
+    func loadCryptoData() {
+        cryptoTrackerData.getCoinList() { (coinList) in
+            self.results = coinList
+            self.coinList = [CoinName](coinList.Data.values)
+            DispatchQueue.main.sync {
+                self.tableView.delegate = self
+                self.tableView.dataSource = self
+                self.searchBar.delegate = self
+                self.searchBar.returnKeyType = .done
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    //SearchBarDelegate
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            isSearching = false
+            view.endEditing(true)
+            tableView.reloadData()
+        } else {
+            isSearching = true
+            filterdCoinList = coinList?.filter({$0.CoinName.localizedCaseInsensitiveContains(searchBar.text!)})
+            tableView.reloadData()
+        }
+    }
+    
+    //TableViewDelegates
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let coins = isSearching ? filterdCoinList?.count : coinList?.count
+       // guard let coins = coinList?.count else { fatalError() }
+        return coins!
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "cryptoCell", for: indexPath)
+        
+        guard var coinList = isSearching ? filterdCoinList : coinList else {fatalError()}
+        coinList = coinList.sorted { $0.CoinName < $1.CoinName }
+        self.sortedCoinList = coinList
+        
+        myCell.textLabel?.text = "\(coinList[indexPath.row].CoinName)"
+        
+//        guard let symbol = self.sortedCoinList?[indexPath.row].Symbol else { fatalError() }
+//            cryptoTrackerData.coinPrice(symbol: symbol) { (coinPrice) in
+//                DispatchQueue.main.sync {
+//                    myCell.imageView?.image = UIImage(data: self.cryptoTrackerData.getImageFromUrl(imageURL: (self.sortedCoinList?[indexPath.row].ImageUrl)!))
+//                }
+//            }
+        return myCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
 }
 
